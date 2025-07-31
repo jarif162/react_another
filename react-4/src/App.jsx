8cd9f2eba90107f9798e0487857040d078d87fbe
Jarif Foysal <jarifsudin6@gmail.com>
2025-08-01 01:00:16 +0600

    hh

--- react-4/src/App.jsx
+++ react-4/src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  // const [todo, setTodo] = useState({});
+  const [userId, setUser] = useState(1);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
 
+  //useEffect(() => {
+  //form
+  useEffect(() => {
+    console.log("useEffect called");
+    fetch(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data);
+        // setTodo(data[0]); // Assuming you want to set the first todo as the current todo
+      });
+    //api call,state change,function call
+  }, [userId]); //dependency array
+
+  const handlePadination = (userId) => {
+    console.log("handlePadination called", userId);
+    setUser(userId);
+  };
+  // console.log(todo);
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    console.log("form submitted");
+  };
+
+  const handleChange = (event) => {
+    event.target.value;
+    console.log("handleChange called", event.target.value);
+  };
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{todos.length}</h1>
+
+        {/* <h1>{todo.title}</h1> */}
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.id} - {todo.title}
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => handlePadination(1)}>1</button>
+        <button onClick={() => handlePadination(2)}>2</button>
+
+        <br />
+
+        <form action="" onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            value={formData.name}
+            onChange={handleChange}
+            type="text"
+            name="name"
+          />
+          <br />
+          <br />
+          <label htmlFor="email">Email</label>
+          <input
+            value={formData.email}
+            onChange={handleChange}
+            type="email"
+            name="email"
+          />
+          <br />
+          <br />
+
+          <label htmlFor="phone">Phone</label>
+          <input
+            value={formData.phone}
+            onChange={handleChange}
+            type="tel"
+            name="phone"
+          />
+          <br />
+          <br />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+//jsonplaceholder-dummyapi
